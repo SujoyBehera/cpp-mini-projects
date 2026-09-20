078b0c2fb1bdb62372dcf0e5828c4801539c5047
Sujoy Behera <beherasujoy@gmail.com>
2026-09-20 16:38:27 +0530

    Add difficulty levels

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -4,12 +4,42 @@
 using namespace std;
 int main(){
     cout<<"====Welcome to the Number Guessing Game!===="<<endl;
-    cout<<"Guess a Number between 1 and 100"<<endl;
-    int attempts = 0;
     srand(time(0));
-    int secretNumber = rand() % 100 + 1;
-    int guess;
+    int secretNumber;
+    int maxAttempts;
+    int choice;
+    cout<<"Choose a difficulty level:"<<endl;
+    cout<<"1. Easy (unlimited attempts)"<<endl;
+    cout<<"2. Medium (10 attempts)"<<endl;
+    cout<<"3. Hard (7 attempts)"<<endl;
+    cin>>choice;
+    switch(choice){
+        case 1:
+            secretNumber = rand() % 50 + 1;
+            cout<<"Select a number between 1 and 50;"<<endl;
+            maxAttempts = - 1;
+            break;
+        case 2:
+            secretNumber = rand() % 100 + 1;
+            cout<<"Select a number between 1 and 100;"<<endl;
+            maxAttempts = 10;
+            break;
+        case 3:
+            secretNumber = rand() % 500 + 1;
+            cout<<"Select a number between 1 and 500;"<<endl;
+            maxAttempts = 7;
+            break;
+        default:
+            cout<<"Invalid choice. exiting the game."<< endl;
+            return 0;
+    }
+    int attempts = 0;
+    int guess = 0;
     while(guess != secretNumber){
+        if(maxAttempts != -1 && attempts >= maxAttempts){
+            cout<<"you have reached the attempt limit. The secret number was "<<secretNumber<<endl;
+            break;
+        }
         cout<<"Enter your guess:";
         cin>>guess;
         attempts++;
