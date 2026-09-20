7bafa5242239b4feafc17ea8dbfec5bfc217c5a2
Sujoy Behera <beherasujoy@gmail.com>
2026-09-21 00:58:42 +0530

    Add Calculator project

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"-----Calculator-----"<<endl;
+    double num1;
+    double num2;
+    char operation;
+    cout<<"Enter the first number:"<<endl;
+    cin>>num1;
+    cout<<"Enter the second number:"<<endl;
+    cin>>num2;
+    
+    return 0;
+}
